ff0bd4ba80b1a96648cbedab9e3936d99988d2e6
Pascal-Benink <9018437@student.zadkine.nl>
2023-02-17 13:52:54 +0100

    17-02-2023 Pascal 001

---
+++ nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <a href="./index.php"><img src="./img/logo.png" alt="" class="nav-logo"></a>
+    <ul>
+        <li><a href="./">----</a></li>
+    </ul>
+    <a href="./login.php"><img src="./img/inlog.png" alt=""></a>
+</nav>
