5d544e3140ef2c587a778d554c089eb68d993f71
FlorisBreemer <9018644@student.zadkine.nl>
2023-02-22 16:38:06 +0100

    begin milieu pagina

--- nav.php
+++ nav.php
@@ -7,7 +7,7 @@
                 <a href="./emp.php">Medewerkers</a>
                 <a href="./Bedrijfinfo.php">Bedrijfsinformatie</a>
                 <a href="./">Retour Policy</a>
-                <a href="./">Milieu</a>
+                <a href="./milieu.php">Milieu</a>
             </div>
         </li>
         <li><a href="./">Producten</a></li>
