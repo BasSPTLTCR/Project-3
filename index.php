a827612b235658cc0388611876f050e477c643d4
EbyvanBokkem <9018376@student.zadkine.nl>
2023-02-22 11:09:03 +0100

    Hoem Page af

--- index.php
+++ index.php
@@ -11,11 +11,38 @@
     <?php
     include "./nav.php";
     ?>
-    <header class="indexheader">
+    <header class="indexheader"></header>
+    <section class="homepagina">
+    <section class="doelstelling">
+        <h1 class="doel">Wat is ons doel met SiRa?</h1>
+        <br><br>
+        <p class="doeltekst">In 2016 begon waren de Koplopers van de Simulatie markt in Nederland, Bol.com en Mediamarkt.
+             in 2022 werd SiRa nummer 1 van Nederland en sindsdien willen wij nummer 1 van de wereld worden.
+             <br>
 
-    </header>
+Om deze doelstelling te bereiken, zou SiRa zich op verschillende strategieën kunnen richten, zoals:
+<br>
+<br>
+Uitbreiding van de verkoopkanalen: Het bedrijf kan zich richten op het uitbreiden van de verkoopkanalen door online advertenties en verkoopplatforms te gebruiken om het bereik van het product te vergroten.
+<br>
+<br>
+Klantenservice verbeteren: Door een uitstekende klantenservice te bieden, kunnen klanten worden aangetrokken en behouden, waardoor de kans groter wordt dat ze SiRa's producten zullen aanbevelen aan anderen.
+<br>
+<br>
+Productkwaliteit en innovatie: Door de kwaliteit en innovatie van de producten te verbeteren, kan SiRa zich differentiëren van de concurrentie en potentiële klanten aantrekken.
+<br>
+<br>
+Marketing en promotie: Het bedrijf kan zich richten op marketing en promotie van haar producten, bijvoorbeeld door deel te nemen aan beurzen, het sponsoren van raceteams, het samenwerken met bekende coureurs, enzovoort.
+<br>
+<br>
+Het is belangrijk dat SiRa haar doelstelling meetbaar en specifiek maakt, zodat het succes kan worden geëvalueerd en bijgestuurd indien nodig. Door zich te richten op deze strategieën en regelmatig de prestaties te meten, kan SiRa op koers blijven om de grootste speler te worden in de markt voor auto-race-simulatoren.
+        
+        </p>
+    </section>
+    </section>
     <?php
     include "./footer.php";
     ?>
+
 </body>
 </html>
