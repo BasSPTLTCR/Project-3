68e3db2af7d96ef04e2a02fa9bf42002015b01da
Pascal-Benink <9018437@student.zadkine.nl>
2023-02-22 10:58:38 +0100

    22-02-2023 Pascal 007

---
+++ com.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include "./nav.php";
+    ?>
+    <header>
+        <h1>Complimenten Formulier</h1>
+    </header>
+    <form action="" method="post">
+        <label for="name">Naam: </label>
+        <input type="text" name="name">
+        <br>
+        <label for="email">E-Mail: </label>
+        <input type="email" name="email">
+        <br>
+        <label for="com">Compliment: </label>
+        <br>
+        <textarea name="com" cols="30" rows="10"></textarea>
+        <br>
+        <input type="submit" value="Verstuur">
+    </form>
+    <?php
+    include "./footer.php";
+    ?>
+</body>
+</html>
