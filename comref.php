833e80da6d6ea23c535399aa9210e85236ee832c
Pascal-Benink <9018437@student.zadkine.nl>
2023-03-06 13:50:37 +0100

    06-03-2023 Pascal 001

---
+++ comref.php
@@ -0,0 +1,35 @@
+<!--Pagina van Pascal-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include "./nav.php";
+    ?>
+    <div class="form-img">
+        <section class="form p">
+            <?php
+            echo "Compliment";
+            echo "<br>";
+            echo "<br>";
+            echo "Naam: " . $_POST['name'];
+            echo "<br>";
+            echo "<br>";
+            echo "E-Mail: " . $_POST['email'];
+            echo "<br>";
+            echo "<br>";
+            echo "Compliment: " . $_POST['com'];
+            ?>
+        </section>
+    </div>
+    <?php
+    include "./footer.php";
+    ?>
+</body>
+</html>
