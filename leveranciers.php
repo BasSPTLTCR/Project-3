6b2724f9082a9dee15f996670b3cc30f72d95879
BasVanDenBlink <9014541@student.zadkine.nl>
2023-03-14 12:41:05 +0100

    14/03/2023 Bas 002

--- leveranciers.php
+++ leveranciers.php
@@ -21,7 +21,45 @@
         #2 querydef
         try
         {
-            $fullQuery = $db->prepare("SELECT name, address, country, phonenumber, email FROM `supplier`");
+            $fullQuery = $db->prepare("SELECT DISTINCT country FROM supplier ORDER BY `supplier`.`country` ASC");
+
+        }
+        catch(PDOExeption $e) 
+        {
+            die("Fout bij verbinden met database: " . $e->getMessage());
+        }
+        #3 querydoen
+        $fullQuery->execute();
+
+        #4 checkresult
+        if ($fullQuery->RowCount() > 0)
+        {
+        $result=$fullQuery->FetchAll(PDO::FETCH_ASSOC);
+
+        #5 show result
+        ?>
+        <form action="" method="post">
+        <select name="country" id="country">
+            <option value="">--- Kies een land ---</option>
+
+                <?php
+                    foreach($result as $rij) 
+                    {
+                        echo "<option>".$rij["country"]."</option>";
+                    }
+                ?>
+            </select>
+            <input type="submit" name="confirm" value="confirm">
+        </form>
+        <?php
+        }
+        
+        if (isset($_POST["confirm"])) {
+            $country = $_POST["country"];
+        }
+        try
+        {
+            $fullQuery = $db->prepare("SELECT name, address, country, phonenumber, email FROM `supplier` WHERE country LIKE '$country%'");
 
         }
         catch(PDOExeption $e) 
