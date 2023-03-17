c0613371a048ab96514d02f7d0dd34e78e53bc87
BasVanDenBlink <9014541@student.zadkine.nl>
2023-03-17 16:22:20 +0100

    17/03/2023 Bas 006

--- producten.php
+++ producten.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,60 +8,56 @@
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     include "./nav.php";
-    ?>
-    <?php
-            #1 verbinding database
-            require './dbconnenct.php';
 
-            #2 querydef
-            try
-            {
-                $fullQuery = $db->prepare("SELECT `name`, price FROM `product`");
+        #1 verbinding database
+        require './dbconnenct.php';
+
+        #2 querydef
+        try
+        {
+            $fullQuery = $db->prepare("SELECT `name`, price FROM `product`");
     
-            }
-            catch(PDOExeption $e) 
-            {
-                die("Fout bij verbinden met database: " . $e->getMessage());
-            }
-            #3 querydoen
-            $fullQuery->execute();
-    
-            #4 checkresult
-            if ($fullQuery->RowCount() > 0)
-            {
-            $result=$fullQuery->FetchAll(PDO::FETCH_ASSOC);
-    
-            #5 show result
-            ?>
+        }
+        catch(PDOExeption $e) 
+        {
+        die("Fout bij verbinden met database: " . $e->getMessage());
+        }
+        #3 querydoen
+        $fullQuery->execute();
+
+        #4 checkresult
+        if ($fullQuery->RowCount() > 0)
+        {
+        $result=$fullQuery->FetchAll(PDO::FETCH_ASSOC);
+
+        #5 show result
             
-                    <?php
-                        echo "<section class='container'>";
+        echo "<section class='container'>";
 
-                        foreach($result as $rij) 
-                        {
-                            echo "<div class='card'>";
-                            echo "<div class='card-image'><img src='./img/product/" . $rij["name"] . ".png' alt=''class='card-image-image'></div>";
-                            echo "<h2>" . $rij["name"] . "</h2>";
-                            echo "<p>" . $rij["price"] . "<p>";
-                            echo "</div>";
-                        }
-                        echo "</section>";
-                    ?>
-    
-            <?php
-            }
-            else
-            {
-                echo "<h2>Sorry,Geen resultaat gevonden</h2>";
-            }
-            #6 geen result melding
-            ?>
-    <?php
+            foreach($result as $rij) 
+                {
+                    echo "<div class='card'>";
+                    echo "<div class='card-image'><img src='./img/product/" . $rij["name"] . ".png' alt=''class='card-image-image'></div>";
+                    echo "<h2>" . $rij["name"] . "</h2>";
+                    echo "<p>" . $rij["price"] . "<p>";
+                    echo "<button class='card-cart'>Add to cart +</button>";
+                    echo "</div>";
+                }
+        echo "</section>";
+        }
+        else
+        {
+            echo "<h2>Sorry,Geen resultaat gevonden</h2>";
+        }
+        #6 geen result melding
+
     include "./footer.php";
     ?>
 
 </body>
+
 </html>
