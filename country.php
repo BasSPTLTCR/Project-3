14afcc61acb7f4f8e04a20d6452cbdfde780f010
FlorisBreemer <9018644@student.zadkine.nl>
2023-03-22 11:31:46 +0100

    country gegevens ding

--- country.php
+++ country.php
@@ -18,89 +18,48 @@
         #1 verbinding database
         require './dbconnenct.php';
 
-        #2 querydef
-        try
-        {
-            $fullQuery = $db->prepare("SELECT DISTINCT country.name AS country FROM `supplier` INNER JOIN country on supplier.country_id = country.idcountry ORDER BY `country`.`name` ASC");
-
-        }
-        catch(PDOExeption $e) 
-        {
-            die("Fout bij verbinden met database: " . $e->getMessage());
-        }
-        #3 querydoen
-        $fullQuery->execute();
-
-        #4 checkresult
-        if ($fullQuery->RowCount() > 0)
-        {
-        $result=$fullQuery->FetchAll(PDO::FETCH_ASSOC);
-
-        #5 show result
         ?>
         <form action="" method="post">
-        <select name="country" id="country">
-            <option value="">--- Kies een land ---</option>
-
-                <?php
-                    foreach($result as $rij) 
-                    {
-                        echo "<option>".$rij["country"]."</option>";
-                    }
-                ?>
-            </select>
+            <input type="text" name="countname" id="countname">
             <input type="submit" name="confirm" value="confirm">
         </form>
         <?php
-            }
-            $country= "";
-            if (isset($_POST["country"])) {
-                $country=  $_POST["country"];
-            }
-            if ($country== "") {
-                $country= "%";
-            }
-        try
-        {
-            $fullQuery = $db->prepare("SELECT supplier.name, supplier.address, country.name AS country, supplier.phonenumber, supplier.email FROM `supplier` INNER JOIN country on supplier.country_id = country.idcountry WHERE country.name LIKE '$country%'");
-
+        $country = "";
+        if (isset($_POST["confirm"])) {
+            $countname = $_POST["countname"];
         }
-        catch(PDOExeption $e) 
-        {
+        try {
+            $fullQuery = $db->prepare("SELECT country.name, country.code FROM `country` WHERE country.name LIKE :countname");
+            $fullQuery->bindValue(':countname', $countname . "%");
+        } catch (PDOExeption $e) {
             die("Fout bij verbinden met database: " . $e->getMessage());
         }
         #3 querydoen
         $fullQuery->execute();
 
         #4 checkresult
-        if ($fullQuery->RowCount() > 0)
-        {
-        $result=$fullQuery->FetchAll(PDO::FETCH_ASSOC);
+        if ($fullQuery->RowCount() > 0) {
+            $result = $fullQuery->FetchAll(PDO::FETCH_ASSOC);
 
-        #5 show result
-        ?>
-        <table class="tafel">
-            <thead>
-                <th>idcountry</th>
-                <th>name</th>
-                <th>code</th>
-            </thead>
-            <tbody>
-                <?php
-                    foreach($result as $rij) 
-                    {
+            #5 show result
+            ?>
+            <table class="tafel">
+                <thead>
+                    <th>name</th>
+                    <th>code</th>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($result as $rij) {
                         echo "<tr><td>" . $rij["name"] . "</td>";
-                        echo "<td>" . $rij["address"] . "</td>";
-                        echo "<td>" . $rij["country"] . "</td>";
+                        echo "<td>" . $rij["code"] . "</td>";
                     }
-                ?>
+                    ?>
 
-            </tbody>
-        </table>
-        <?php
-        }
-        else
-        {
+                </tbody>
+            </table>
+            <?php
+        } else {
             echo "<h2>Sorry,Geen resultaat gevonden</h2>";
         }
         #6 geen result melding
