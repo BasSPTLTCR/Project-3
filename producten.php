ed6c90b67cf04a4252fcfe9b374f7d9674bf6c59
Pascal-Benink <9018437@student.zadkine.nl>
2023-04-04 12:35:55 +0200

    .

---
+++ producten.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include "./nav.php";
+
+        #1 verbinding database
+        require './dbconnenct.php';
+
+        #2 querydef
+        try
+        {
+            $fullQuery = $db->prepare("SELECT `name`, price FROM `product`");
+    
+        }
+        catch(PDOExeption $e) 
+        {
+        die("Fout bij verbinden met database: " . $e->getMessage());
+        }
+        #3 querydoen
+        $fullQuery->execute();
+
+        #4 checkresult
+        if ($fullQuery->RowCount() > 0)
+        {
+        $result=$fullQuery->FetchAll(PDO::FETCH_ASSOC);
+
+        #5 show result
+            
+        echo "<section class='container'>";
+
+            foreach($result as $rij) 
+                {
+                    $search = array("/", ":");
+                    $replace = array("-", "");
+                    $arr = $rij["name"];
+                    $product = (str_replace($search,$replace,$arr));
+
+                    echo "<div class='card'>";
+                    echo "<div class='card-image'><img src='./img/product/" . $product . ".png' alt=''class='card-image-image'></div>";
+                    echo "<h2>" . $rij["name"] . "</h2>";
+                    echo "<p>" ."€" .  $rij["price"] . "<p>";
+                    echo "<button class='card-cart'>Add to cart +</button>";
+                    echo "</div>";
+                }
+        echo "</section>";
+        }
+        else
+        {
+            echo "<h2>Sorry,Geen resultaat gevonden</h2>";
+        }
+        #6 geen result melding
+
+    include "./footer.php";
+    ?>
+
+</body>
+
+</html>
