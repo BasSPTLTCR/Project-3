bd994994fc583310c9b4c3c8bae89f5719661ca1
Pascal-Benink <9018437@student.zadkine.nl>
2023-04-12 10:58:06 +0200

    12-04-2023 Pascal 003

--- producten.php
+++ producten.php
@@ -16,26 +16,20 @@
         #1 verbinding database
         require './dbconnenct.php';
 
-        #2 querydef
         try
         {
-            $fullQuery = $db->prepare("SELECT `name`, price FROM `product`");
             $oneQuery = $db->prepare("SELECT name as CatName FROM `category` ORDER BY `CatName` ASC;");
-    
         }
         catch(PDOExeption $e) 
         {
-        die("Fout bij verbinden met database: " . $e->getMessage());
+            die("Fout bij verbinden met database: " . $e->getMessage());
         }
         #3 querydoen
-        $fullQuery->execute();
         $oneQuery->execute();
 
         #4 checkresult
-        if ($fullQuery->RowCount() > 0 && $oneQuery->RowCount() > 0)
-        {
-        $result=$fullQuery->FetchAll(PDO::FETCH_ASSOC);
-        $result2=$oneQuery->FetchAll(PDO::FETCH_ASSOC);
+        if ($oneQuery->RowCount() > 0)
+        $result=$oneQuery->FetchAll(PDO::FETCH_ASSOC);
 
         #5 show result
         ?>
@@ -44,28 +38,60 @@
             <option value="">--- Category ---</option>
 
                 <?php
-                    foreach($result2 as $rij2) 
+                    foreach($result as $rij) 
                     {
-                        echo "<option>".$rij2["CatName"]."</option>";
+                        echo "<option>".$rij["CatName"]."</option>";
                     }
                 ?>
             </select>
-            <input type="text" name="search2" id="search2">
+            <div>
+                <label for="search">Product Naam:</label>
+                <input type="text" name="search" id="search">
+            </div>
+            <div>
+                <label for="search_lev">Leverancier Naam:</label>
+                <input type="text" name="search_lev" id="search_lev">
+            </div>
             <input type="submit" name="confirm" value="confirm">
         </form>
         <?php
                 $CatName= "%";
-                $search2= "%";
+                $search= "%";
+                $search_lev= "%";
             if (isset($_POST["confirm"])) {
                 $CatName= $_POST["CatName"];
-                $search2= "%" . $_POST["search2"] . "%";
+                $search= "%" . $_POST["search"] . "%";
+                $search_lev= "%" . $_POST["search_lev"] . "%";
                 if ($CatName == "") {
                     $CatName= "%";
                 }
-                if ($search2 == "") {
-                    $search2= "%";
+                if ($search == "") {
+                    $search= "%";
+                }
+                if ($search_lev == "") {
+                    $search_lev= "%";
                 }
             }
+
+            #2 querydef
+            try
+            {
+                $fullQuery = $db->prepare("SELECT product.name AS name, product.price, supplier.name AS SupplierName, category.name AS CategoryName FROM `product` INNER JOIN supplier ON product.supplier_id = supplier.id INNER JOIN category on product.category_id = category.id WHERE product.name LIKE :search AND category.name LIKE :CatName AND supplier.name LIKE :search_lev ;");
+                $fullQuery->bindValue(':search',$search);
+                $fullQuery->bindValue(':CatName', $CatName);
+                $fullQuery->bindValue(':search_lev', $search_lev);
+            }
+            catch(PDOExeption $e) 
+            {
+                die("Fout bij verbinden met database: " . $e->getMessage());
+            }
+            #3 querydoen
+            $fullQuery->execute();
+    
+            #4 checkresult
+            if ($fullQuery->RowCount() > 0)
+            {
+            $result=$fullQuery->FetchAll(PDO::FETCH_ASSOC);
 
         echo "<section class='container'>";
 
